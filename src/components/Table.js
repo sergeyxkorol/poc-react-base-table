0052f0765eb3ae72957324aff16d39bbbcfda606
Siarhei <xkorolx@gmail.com>
2020-02-04 17:08:30 +0300

    Add fixed columns Add sorting

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,12 +1,44 @@
-import React from "react";
-import BaseTable from "react-base-table";
+import React, { useState } from "react";
+import BaseTable, { Column, SortOrder } from "react-base-table";
 import { generateColumns, generateData } from "../utils";
 import "react-base-table/styles.css";
 import "./Table.scss";
 
 export default function Table() {
-  const columns = generateColumns(6);
-  const data = generateData(columns, 200);
+  const columns = generateColumns(10);
+  const data = generateData(columns, 500);
 
-  return <BaseTable columns={columns} data={data} width={600} height={400} />;
+  const fixedColumns = columns.map((column, columnIndex) => {
+    let frozen;
+    if (columnIndex < 1) frozen = Column.FrozenDirection.LEFT;
+    return {
+      ...column,
+      frozen,
+      resizable: true,
+      sortable: true,
+      maxWidth: 300
+    };
+  });
+
+  const defaultSort = { key: "column-0", order: SortOrder.ASC };
+
+  const [sortedData, setSortedData] = useState(data);
+  const [sortBy, setSortBy] = useState(defaultSort);
+
+  const onColumnSort = sortBy => {
+    setSortBy(sortBy);
+    setSortedData(sortedData.reverse());
+  };
+
+  return (
+    <BaseTable
+      fixed
+      columns={fixedColumns}
+      data={sortedData}
+      width={1000}
+      height={400}
+      sortBy={sortBy}
+      onColumnSort={onColumnSort}
+    />
+  );
 }
