d7eb9337ae72a5a09115633c467fee085be28316
Siarhei <xkorolx@gmail.com>
2020-02-04 17:31:16 +0300

    Updates

---
+++ precache-manifest.7fde8ba27024d8b5c70284095400466c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37bad564e458a4b3314b10f19c27b573",
+    "url": "/poc-react-base-table/index.html"
+  },
+  {
+    "revision": "ec387fef88b312e7c8dd",
+    "url": "/poc-react-base-table/static/css/2.641454a3.chunk.css"
+  },
+  {
+    "revision": "b89667f2757b4f74ad1a",
+    "url": "/poc-react-base-table/static/css/main.e64a3231.chunk.css"
+  },
+  {
+    "revision": "ec387fef88b312e7c8dd",
+    "url": "/poc-react-base-table/static/js/2.0324425f.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/poc-react-base-table/static/js/2.0324425f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b89667f2757b4f74ad1a",
+    "url": "/poc-react-base-table/static/js/main.d1dd8270.chunk.js"
+  },
+  {
+    "revision": "d08bed6b9b6309337a42",
+    "url": "/poc-react-base-table/static/js/runtime-main.d836cf94.js"
+  }
+]);
