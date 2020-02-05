014c0358270a0eeda4f219421e84089e1c2886a6
Sergey Korol <xkorolx@gmail.com>
2020-02-05 14:10:36 +0300

    Add cells editing

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { useState, useMemo } from "react";
 import { Column } from "react-base-table";
 import Table from "./components/Table";
 import Filter from "./components/Filter";
+import EditableCell from "./components/EditableCell";
 import { generateColumns } from "./utils";
 import "./App.scss";
 
 function App() {
-  const [columns, setColumns] = useState(generateColumns(10));
+  const generatedColumns = generateColumns(10);
+  generatedColumns[1].cellRenderer = EditableCell;
+
+  const [columns, setColumns] = useState(generatedColumns);
 
   function handleFilter(columnKey, value) {
     const updatedColumns = columns.map(col => {
